2e4ba543a0d7d2b544a179e1d477e350f378736c
dly <2011429436@qq.com>
2021-06-18 11:15:07 +0800

    最小好进制

--- SuanFa/leetcode/HelloWorld.java
+++ SuanFa/leetcode/HelloWorld.java
@@ -5,8 +5,14 @@ import leetcode.hard.Demo1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Demo1 dd = new Demo1();
-        String a = "1000000";
-        System.out.println("你好世界！！！"+dd.smallestGoodBase(a));
+//        Demo1 dd = new Demo1();
+//        String a = "1000000";
+//        System.out.println("你好世界！！！"+dd.smallestGoodBase(a));
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 13; j++) {
+                System.out.print("("+i+","+j+")");
+            }
+            System.out.println();
+        }
     }
 }
--- SuanFa/leetcode/hard/Demo1.java
+++ SuanFa/leetcode/hard/Demo1.java
@@ -5,8 +5,10 @@ public class Demo1 {
         long nVal = Long.parseLong(n);
 //        将第一个参数用第二个参数进制来表示，如果不写第二个参数的话默认是十进制
         int mMax = (int)Math.floor(Math.log(nVal)/Math.log(2));
+//        mMax是最大的位数
         for (int m = mMax; m > 1 ; m--) {
             int k = (int)Math.pow(nVal, 1.0 / m);
+//            对应位的进制数
             long mul = 1,sum = 1;
             for (int i = 0; i < m; i++) {
                 mul *= k;
@@ -14,10 +16,11 @@ public class Demo1 {
             }
             if(sum==nVal){
                 return Integer.toString(k);
+//                计算成功，返回可行的进制数
             }
         }
         return Long.toString(nVal-1);
-//        返回的是所选进制
+//        返回的是输入数-1，把它作为进制数a，最低位是a的0次方，等于1
 //        char要用'';String要用""
     }
 }
