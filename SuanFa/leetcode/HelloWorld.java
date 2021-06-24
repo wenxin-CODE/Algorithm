1fb7c2b6c662ea837f0e15c55c147339b6ae31db
dly <2011429436@qq.com>
2021-06-24 19:10:10 +0800

    笔记

--- SuanFa/leetcode/HelloWorld.java
+++ SuanFa/leetcode/HelloWorld.java
@@ -1,15 +1,14 @@
 package leetcode;
 
 import leetcode.easy.Demo4;
-//import leetcode.hard.Demo1;
-import leetcode.middle.Demo1;
+import leetcode.hard.Demo2;
+//import leetcode.middle.Demo2;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Demo1 dd = new Demo1();
-        String a = "1000000";
-        dd.movingCount(25,36,1);
-        System.out.println("你好世界！！！"+dd.movingCount(25,36,1));
+        Demo2 dd = new Demo2();
+        int[][] arr = {{1,3,5,4,2,1},{1,2,3,1,3,4}};
+        System.out.println("你好世界！！！"+dd.maxPoints(arr));
 //        for (int i = 0; i < 10; i++) {
 //            for (int j = 0; j < 13; j++) {
 //                System.out.print("("+i+","+j+")");
---
+++ SuanFa/leetcode/middle/Demo2.java
@@ -0,0 +1,42 @@
+package leetcode.middle;
+
+import java.util.*;
+
+//https://leetcode-cn.com/problems/throne-inheritance/solution/huang-wei-ji-cheng-shun-xu-by-leetcode-s-p6lk/
+public class Demo2 {
+    Map<String, List<String>> edges;
+    Set<String> dead;
+    String king;
+
+    public void Demo2(String kingName) {
+        edges = new HashMap<String, List<String>>();
+        dead = new HashSet<String>();
+        king = kingName;
+    }
+
+    public void birth(String parentName, String childName) {
+        List<String> children = edges.getOrDefault(parentName, new ArrayList<String>());
+        children.add(childName);
+        edges.put(parentName, children);
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> ans = new ArrayList<String>();
+        preorder(ans, king);
+        return ans;
+    }
+
+    private void preorder(List<String> ans, String name) {
+        if (!dead.contains(name)) {
+            ans.add(name);
+        }
+        List<String> children = edges.getOrDefault(name, new ArrayList<String>());
+        for (String childName : children) {
+            preorder(ans, childName);
+        }
+    }
+}
