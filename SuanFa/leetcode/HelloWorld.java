7ab6e52b8f8ae253a27e870242dcfd3e78a9d110
dly <2011429436@qq.com>
2021-06-30 16:35:32 +0800

    序列化二叉树

--- SuanFa/leetcode/HelloWorld.java
+++ SuanFa/leetcode/HelloWorld.java
@@ -1,14 +1,15 @@
 package leetcode;
 
-import leetcode.easy.Demo4;
-import leetcode.hard.Demo3;
+//import leetcode.easy.Demo4;
+import leetcode.hard.Demo4;
 //import leetcode.middle.Demo2;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Demo3 dd = new Demo3();
+        Demo4 dd = new Demo4();
         int[][] arr = {{3,2,4},{1,5,0}};
-        System.out.println("你好世界！！！"+dd.slidingPuzzle(arr));
+        System.out.println("你好世界！！！"+dd.deserialize("1,2,3,None,None,4,None,None,5,None,None"));
+        //System.out.println("你好世界！！！"+dd.serialize(dd));
 //        for (int i = 0; i < 10; i++) {
 //            for (int j = 0; j < 13; j++) {
 //                System.out.print("("+i+","+j+")");
