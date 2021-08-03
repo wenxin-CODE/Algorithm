dc843af6620b0a722fee7e701088c31a790d2cfb
dly <2011429436@qq.com>
2021-08-03 15:53:23 +0800

    最短无序连续子数组

--- SuanFa/leetcode/HelloWorld.java
+++ SuanFa/leetcode/HelloWorld.java
@@ -1,8 +1,8 @@
 package leetcode;
 
-import leetcode.easy.Demo9;
+//import leetcode.easy.Demo9;
 //import leetcode.hard.Demo4;
-import leetcode.middle.Demo8;
+import leetcode.middle.Demo9;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -12,9 +12,10 @@ import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Demo8 dd = new Demo8();
-        int[][] nums = {{2,1,1},{2,3,1},{3,4,1}};
-        int k=5;
-        System.out.println(dd.networkDelayTime(nums,4,2));
+        Demo9 dd = new Demo9();
+        int[] nums = {2,6,4,8,10,9,15};
+
+        System.out.println(dd.findUnsortedSubarray(nums));
+//        System.out.println(nums[0]);
     }
 }
---
+++ SuanFa/leetcode/middle/Demo9.java
@@ -0,0 +1,33 @@
+package leetcode.middle;
+
+import java.util.Arrays;
+
+public class Demo9 {
+    public int findUnsortedSubarray(int[] nums){
+        if (isSorted(nums)){
+            return 0;
+        }
+        int[] numsSorted = new int[nums.length];
+        System.arraycopy(nums,0,numsSorted,0,nums.length);
+//        刷意思
+        Arrays.sort(numsSorted);
+        int left = 0;
+        while (nums[left] == numsSorted[left]){
+            left++;
+        }
+        int right = nums.length-1;
+        while (nums[right] == numsSorted[right]){
+            right--;
+        }
+        return right-left+1;
+    }
+
+    public boolean isSorted(int[] nums){
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
