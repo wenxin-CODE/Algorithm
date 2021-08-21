566930d68251da269fbd47bd13692753c52f7b9d
dly <2011429436@qq.com>
2021-08-21 11:45:12 +0800

    压缩字符串

--- SuanFa/leetcode/HelloWorld.java
+++ SuanFa/leetcode/HelloWorld.java
@@ -2,7 +2,7 @@ package leetcode;
 
 //import leetcode.easy.Demo9;
 import leetcode.hard.Demo7;
-import leetcode.middle.Demo12;
+import leetcode.middle.Demo13;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -12,10 +12,10 @@ import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Demo7 dd = new Demo7();
-        int[] nums = {2,4,6,8,10};
+        Demo13 dd = new Demo13();
+        char[] nums = {'a','a','b','b','b','c','c','c'};
 
-        System.out.println(dd.checkRecord(2));
+        System.out.println(dd.compress(nums));
 //        System.out.println(nums[0]);
     }
 }
