83ba5dea145f2b27536d4a80f887fb623c9a51b6
dly <2011429436@qq.com>
2021-08-28 16:32:58 +0800

    检查是否所有整数都被覆盖

--- SuanFa/leetcode/easy/Demo6.java
+++ SuanFa/leetcode/easy/Demo6.java
@@ -2,6 +2,8 @@ package leetcode.easy;
 
 //https://leetcode-cn.com/problems/check-if-all-the-integers-in-a-range-are-covered/solution/jian-cha-shi-fou-qu-yu-nei-suo-you-zheng-5hib/
 //使用差分数组
+//自己列一个算[1,2],[3,4],[5,6]--(2,5)
+//开头只加不减，如果要求区间全覆盖了，前面减的，后面会加上，总和不会小于等于0
 public class Demo6 {
     public boolean isCovered(int[][] ranges,int left,int right){
         int[] diff = new int[52];
