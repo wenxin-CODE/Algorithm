72a89716619fae0cf55e64d2a4243f57e896f820
dly <2011429436@qq.com>
2021-09-08 20:26:52 +0800

    IPO

--- SuanFa/leetcode/HelloWorld.java
+++ SuanFa/leetcode/HelloWorld.java
@@ -1,21 +1,17 @@
 package leetcode;
 
 import leetcode.easy.Demo15;
-import leetcode.hard.Demo7;
+import leetcode.hard.Demo9;
 import leetcode.middle.Demo18;
 
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Demo15 dd = new Demo15();
-        int[] nums = {1,2,3,-3,5,6,7,12,9};
+        Demo9 dd = new Demo9();
+        int[] nums ={1,2,3};
+        int[] capital = {0,1,2};
 
-        System.out.println(dd.balanceStringSplit("LLLRRRLRLRLLLRRRLLLRRRLLRRLRLR"));
+        System.out.println(dd.findMaximizedCapital(3,0,nums,capital));
 //        System.out.println((char) (1 % 10 + '0'));
     }
 }
