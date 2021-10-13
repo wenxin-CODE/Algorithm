44de61cfc46bcd9c594ee521e41957a08f7a0286
‘mike’ <mike_s@163.com>
2021-10-13 19:20:17 +0800

    Fizz Buzz

---
+++ SuanFa/leetcode/easy/Demo16.java
@@ -0,0 +1,29 @@
+package leetcode.easy;
+
+//https://leetcode-cn.com/problems/fizz-buzz/solution/fizz-buzz-by-leetcode-solution-s0s5/
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo16 {
+
+        public List<String> fizzBuzz(int n) {
+            List<String> answer = new ArrayList<String>();
+            for (int i = 1; i <= n; i++) {
+                StringBuffer sb = new StringBuffer();
+                if (i % 3 == 0) {
+                    sb.append("Fizz");
+                }
+                if (i % 5 == 0) {
+                    sb.append("Buzz");
+                }
+                if (sb.length() == 0) {
+                    sb.append(i);
+                }
+                answer.add(sb.toString());
+            }
+            return answer;
+        }
+
+}
